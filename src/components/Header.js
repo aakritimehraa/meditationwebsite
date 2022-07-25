14b7571ee4d41920d65819235bd41fbf0ec65db5
aakritimehraa <aakritimehrasa@gmail.com>
2022-07-25 18:19:36 +0530

    corrected header color

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,7 +34,7 @@ function Header() {
         </div>
         : null
       } 
-      <div className='hidden sm:block header fixed top-0 left-0 z-10   bg-white dark:bg-[#03060b] w-[100%]  sm:flex justify-center align-center '>
+      <div className='hidden sm:block header fixed top-0 left-0 z-10 dark:bg-[#03060b] w-[100%]  sm:flex justify-center align-center '>
       <div id='header' className=' w-[100%] md:w-[60%]  sm:flex justify-center align-center pt-4  md:text-[1rem] mb-5  lg:text-[1.25rem]'>
         <h1 className='py-2 w-[20%]' style={{ fontFamily: 'Josefin Sans' }}>A<sub>M</sub></h1>
         <ul className='flex justify-between items-center pr-10 ml-[20px] mr-[20px] w-[90%] relative' style={{ fontFamily: 'Belleza' }} >
