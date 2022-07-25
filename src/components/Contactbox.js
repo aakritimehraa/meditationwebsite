eb77df7fa1805047b6a15b71f7291e12c0c4f772
aakritimehraa <aakritimehrasa@gmail.com>
2022-07-25 12:49:39 +0530

    corrected forms error

--- src/components/Contactbox.js
+++ src/components/Contactbox.js
@@ -18,6 +18,7 @@ function Contactbox() {
                 <h1 className='text-2xl font-semibold md:text-4xl xl:text-6xl mb-20 dark:text-white'>Love to hear from you <br></br>
                     Get in Touch 👋</h1>
                  <form className='flex justify-center items-center flex-col' name="contact" method="POST" data-netlify="true">
+                 <input type="hidden" name="form-name" value="contact" />.
                 <label className='text-xl mb-2'>Your Name</label>
                 <input name='name' className='dark:bg-gray-900 bg-gray-100 outline-none border-none h-[50px] w-[90%] sm:w-[80%] lg:w-[90%]' type='text' placeholder='name' />
                 <br></br>
@@ -33,28 +34,7 @@ function Contactbox() {
                 </button>
                 </form>
             </div>
-            <form name="contact" method="POST" data-netlify="true">
-            <input type="hidden" name="form-name" value="contact" />
-
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+            
             <div className='hidden xl:block mt-10'>
 
                 <ul className='flex justify-start items-start flex-col dark:text-white'>
