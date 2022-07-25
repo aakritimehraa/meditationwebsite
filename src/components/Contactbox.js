e4357c366bb5998ea60149707431f82cdd573d8c
aakritimehraa <aakritimehrasa@gmail.com>
2022-07-25 12:43:37 +0530

    corrected forms error

--- src/components/Contactbox.js
+++ src/components/Contactbox.js
@@ -33,6 +33,28 @@ function Contactbox() {
                 </button>
                 </form>
             </div>
+            <form name="contact" method="POST" data-netlify="true">
+            <input type="hidden" name="form-name" value="contact" />
+
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
             <div className='hidden xl:block mt-10'>
 
                 <ul className='flex justify-start items-start flex-col dark:text-white'>
